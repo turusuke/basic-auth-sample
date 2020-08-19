7134f7e2b04fa9916b8c5287260f1e8d6b6c20d0
turusuke <mail@turusuke.com>
2020-08-19 18:06:21 +0900

    fix: nits

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
-const path = require('path')
-const port = process.env.PORT || 5000
-const staticRoot = process.env.ROOT || 'public'
-const USER_NAME = process.env.USER_NAME || 'user'
+const protect = require('static-auth');
+const safeCompare = require('safe-compare');
+
+const USER_NAME = process.env.USER_NAME || 'user';
 const PASSWORD = process.env.PASSWORD || 'password';
 
-const fastify = require('fastify')({
-  logger: true,
-})
-
-function validate(username, password, req, reply, done) {
-  if (username === USER_NAME && password === PASSWORD) {
-    done()
-  } else {
-    done(new Error('Winter is coming'))
+const app = protect(
+  '/',
+  (username, password) => safeCompare(username, USER_NAME) && safeCompare(password, PASSWORD),
+  {
+    directory: `${__dirname}/public`,
+    realm: 'now-basic-auth.node-static-auth',
+    onAuthFailed: (res) => {
+      res.end('Authentication failed')
+    },
   }
-}
+)
 
-fastify.register(require('fastify-basic-auth'), {
-  validate,
-  authenticate: true,
-})
-
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, staticRoot),
-})
-
-fastify.listen(port, (err, address) => {
-  if (err) throw err
-  fastify.log.info(`server listening on ${address}`)
-})
-
-fastify.after(() => {
-  fastify.addHook('onRequest', fastify.basicAuth)
-})
+module.exports = app
